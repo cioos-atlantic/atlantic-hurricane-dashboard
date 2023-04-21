2e4d225cac57ad8c60b092125e9bf34c3345e20e
Jeff Cullis <jcullis@dal.ca>
2023-04-21 14:17:26 -0300

    Add function to download a singe shapefile from ECCC.

--- atlantic_hurricane/eccc_shapefiles.py
+++ atlantic_hurricane/eccc_shapefiles.py
@@ -10,7 +10,7 @@ def create_shape_dir():
     if not os.path.exists(SHAPE_DIR):
         os.makedirs(SHAPE_DIR)
 
-def dl_shapefiles():
+def dl_all_shapefiles():
     create_shape_dir()
     r = requests.get(FIONA_URL)
     data = bs4.BeautifulSoup(r.text, "html.parser")
@@ -23,4 +23,21 @@ def dl_shapefiles():
             file_path = os.path.join(SHAPE_DIR, file_name)
             if not os.path.exists(file_path):
                 open(file_path, 'wb').write(r.content)
+
+def dl_shapefile(shapefile_name):
+    create_shape_dir()
+    r = requests.get(FIONA_URL)
+    data = bs4.BeautifulSoup(r.text, "html.parser")
+    links = data.find_all("a")
+    shapefile_href_list = [l["href"] for l in links if l["href"] == shapefile_name]
+    if len(shapefile_href_list) == 1:
+        shapefile_href = shapefile_href_list[0]
+        r = requests.get(FIONA_URL + shapefile_href)
+        u = urlparse(r.url)
+        file_name = os.path.basename(u.path)
+        if file_name:
+            print(file_name)
+            file_path = os.path.join(SHAPE_DIR, file_name)
+            if not os.path.exists(file_path):
+                open(file_path, 'wb').write(r.content)                
 
