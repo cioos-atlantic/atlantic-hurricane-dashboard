70ee495513b2410db7becb3259b521e3eac9849e
JaredMcLellan <jaredamclellan@gmail.com>
2023-06-20 17:48:53 -0300

    Updated IBTrACS data retrieval code

--- IBTrACS/populate_ibtracs_data.py
+++ IBTrACS/populate_ibtracs_data.py
@@ -11,36 +11,34 @@ import requests
 from datetime import datetime
 import pytz
 
-# file in data/ibtracs
-
 # TODO: Find out if the URL will be changed
-# Can possibly just pattern match the NA section
+# Store in config
 url = "https://www.ncei.noaa.gov/data/international-best-track-archive-for-climate-stewardship-ibtracs/v04r00/access/csv/ibtracs.NA.list.v04r00.csv"
-existing_data = True
+history_file = "last-modified.txt"
 
-f = open("data/last-modified.txt", "r")
-if(f.read()==''):
-    print("No existing data grab found")
-    existing_data = False
-else:
-    x = requests.head(url)
-    existing_date = datetime.strptime(f.read(), '%a, %d %b %Y %H:%M:%S %Z')
-    incoming_date = datetime.strptime(x.headers['last-modified'], '%a, %d %b %Y %H:%M:%S %Z')
-    x.close
+if(not os.path.isfile(history_file)):
+    f= open(history_file, "x")
+f = open(history_file, "r")
+last_modified = f.read()
+x = requests.head(url)
+incoming_date = datetime.strptime(x.headers['last-modified'], '%a, %d %b %Y %H:%M:%S %Z')
+if(last_modified):
+    existing_date = datetime.strptime(last_modified, '%a, %d %b %Y %H:%M:%S %Z')
+x.close
 
 columns = ['SID', 'SEASON', 'BASIN', 'SUBBASIN', 'NAME', 'ISO_TIME', 'NATURE', 'LAT', 'LON', 
 'WMO_WIND', 'WMO_PRES', 'WMO_AGENCY', 'IFLAG']
 
-if(existing_date < incoming_date):
-    print("Newer data exists. Downloading")
-    
+if(not last_modified or existing_date < incoming_date):
     df = pd.read_csv(url, usecols=columns)
-    df = pd.read_csv("data/ibtracs.NA.list.v04r00.csv", usecols = columns)
     df['ISO_TIME'] = pd.to_datetime(df['ISO_TIME'], format = '%Y-%m-%d %H:%M:%S', errors='coerce')
-    new_data = df[df['ISO_TIME'] > existing_date]
+    if(last_modified):
+        df = df[df['ISO_TIME'] > existing_date]
 
-    f = open("data/last-modified.txt", "w")
+    f = open(history_file, "w")
     f.write(x.headers['last-modified'])
     f.close()
+
+    print("Import successful")
 else:
     print("Newest data already downloaded")
