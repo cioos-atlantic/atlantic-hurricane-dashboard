671ebe386b1063dc086835c69fe0cd4ecfc4a356
sjbruce <scott.bruce@mi.mun.ca>
2024-09-03 13:41:46 -0230

    updated file watch to process each file individually updated minor issue with doc

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 Some sample notebooks can be found in the jupyter subfolder. You will first need to create the 'hurricane' conda environmnent:
 
 ```
-conda create -f environment.yml
+conda env create -f environment.yml
 ```
 
 Then set VS Code to use the hurricane kernal and run through the notebooks.
--- sarracenia config_file/file_watcher.py
+++ sarracenia config_file/file_watcher.py
@@ -1,4 +1,3 @@
-# %%
 # Using the watchdog library with filehandler
 # this script is combined with the generate_bound script to watch for new files 
 # in the set directory and generate the boundary requirements for ERDDAP to query
@@ -20,10 +19,6 @@ logging.basicConfig(
         datefmt='%Y-%m-%dT%H:%M:%S')
 
 logger= logging.getLogger(__name__)
-
-load_dotenv() 
-
-folder_path = os.getenv('ECCC_SHP_SOURCE')
 
 def on_created(event):
     # This occurs when a new file has been added or created
@@ -48,7 +43,7 @@ def on_created(event):
         logger.info("Uploading Data to PostGIS Database!")
         engine = lpg.pg_engine()
 
-        lpg.process_eccc_shp_files(source_path, engine)
+        lpg.process_eccc_shp(event.src_path, engine)
         
         engine.dispose()
 
@@ -80,36 +75,37 @@ def on_moved(event):
     logger.debug(event.src_path)
 
 if __name__ == "__main__":
-    
-    
-    event_handler = FileSystemEventHandler()
-    #calling functions
-    event_handler.on_created = on_created
-    event_handler.on_deleted = on_deleted
-    event_handler.on_modified = on_modified
-    event_handler.on_moved = on_moved
+    load_dotenv() 
 
-    #set to watch the shapefiles directory
-    #path = os.path.abspath(os.path.join('..', 'shapefiles')) # to be configured
-    #path= r"/path/to/shapefiles/" # test path
-    path= folder_path
+    folder_path = os.getenv('ECCC_SHP_SOURCE')
+
+    event_handler = FileSystemEventHandler()
+    
+    event_handler.on_created = on_created
+    # event_handler.on_deleted = on_deleted
+    # event_handler.on_modified = on_modified
+    # event_handler.on_moved = on_moved
     
     observer = Observer()
-    observer.schedule(event_handler, path, recursive=True) # recursive means it is monitoring everything in the sub folders so it should be left at True
+    
+    # recursive means it is monitoring everything in the sub folders so it should be left at True
+    observer.schedule(event_handler, folder_path, recursive=True)
     observer.start()
+
     try:
-        logger.info ("Now Monitoring")
-        while True:
-            time.sleep(30)
+        logger.info("Now Monitoring")
+        while observer.is_alive():
+            time.sleep(10)
 
             if os.path.exists(os.getenv('STOP_FILE')):
-                logger.info("STOP FILE detected, ")
-                break
+                observer.stop()
+                logger.info("STOP FILE detected, exiting...")
             
     except KeyboardInterrupt:
         observer.stop()
         logger.info("Monitoring Interrupted")
+
     observer.join()
-    
+
 
 
